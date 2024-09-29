662177224207bae54a99e809a9314fb0ddc8bb3b
boodac <180095661+Boodac@users.noreply.github.com>
2024-09-29 10:39:28 -0400

    Remove dump() import from source.js

--- src/source.js
+++ src/source.js
@@ -2,7 +2,7 @@ import "./assets/styles.css";
 import initHome from "./assets/home.js";
 import initContact from "./assets/contact.js";
 import initMenu from "./assets/menu.js";
-import { default as hook, buttons, dump } from "./assets/eventhandling.js";
+import { default as hook, buttons } from "./assets/eventhandling.js";
 
 const content = document.querySelector("#content");
 
