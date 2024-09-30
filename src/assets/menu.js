a6cd23af1929663cc4ccc6290dd6242885a2f4be
boodac <180095661+Boodac@users.noreply.github.com>
2024-09-30 14:35:50 -0400

    Add createSection() and createOption(), utilizing the menu object from config.js

--- src/assets/menu.js
+++ src/assets/menu.js
@@ -1,13 +1,41 @@
+import { menu } from "./config.js";
+
 const title = document.createElement("h1");
-title.textContent = "Menu";
-const copy = document.createElement("p");
-copy.textContent = "Grilled Cheese $5";
+title.textContent = menu.meta.title;
 
 const initMenu = (container) => {
     container.appendChild(title);
-    container.appendChild(copy);
+    menu.structure.forEach(section => {
+        container.appendChild(createSection(menu[section]));
+    });
+}
+
+function createSection(sectionObject) {
+    const section = document.createElement("section");
+    section.classList.add("menu-section");
+    const sectionHeader = document.createElement("h2");
+    sectionHeader.classList.add("section-title");
+    sectionObject.options.forEach(item => {
+        section.appendChild(createOption(item));
+    });
+    return section;
+}
+
+function createOption(optionObject) {
+    const price = document.createElement("span");
+    price.classList.add("item-price");
+    price.textContent = optionObject.price;
+    const name = document.createElement("span");
+    name.classList.add("item-name");
+    name.textContent = optionObject.name
+    const description = document.createElement("p");
+    description.classList.add("item-description");
+    description.textContent = optionObject.description;
+    const card = document.createElement("div");
+    card.appendChild(name);
+    card.appendChild(price);
+    card.appendChild(description);
+    return card;
 }
 
 export default initMenu;
-
-export { title as tagline, copy };
